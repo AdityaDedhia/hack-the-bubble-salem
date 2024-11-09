0ad9fe54109973b6049e6d0050087a9cdfacb481
Aditya Dedhia <aditya23dedhia@gmail.com>
2024-11-09 14:57:37 +0000

    Added new url.

--- salem/app/urls.py
+++ salem/app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home, add_client, get_clients_size, main
+from .views import home, add_client, get_clients_size, main, attempt_assign_role
 
 app_name = "app"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("add_client", add_client, name="add_client"),
     path("get_clients_size", get_clients_size, name="get_clients_size"),
     path("main", main, name="main"),
+    path("attempt_assign_role", attempt_assign_role, name="attempt_assign_role"),
 ]
