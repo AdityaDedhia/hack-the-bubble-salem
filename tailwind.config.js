1b8b4534e2916bd1d86f6fd05e0edb7aa68ba27e
S-andres0694 <sebasandres0694@gmail.com>
2024-11-09 10:54:24 +0000

    deleted JS

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./salem/app/templates/app/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
