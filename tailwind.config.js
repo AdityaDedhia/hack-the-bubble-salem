40562fa1c1bca7ffa873f449a7e6447e2abe3c96
S-andres0694 <sebasandres0694@gmail.com>
2024-11-09 10:42:27 +0000

    push

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./salem/app/templates/app/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
